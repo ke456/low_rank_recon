6bbc0eb2fc58773d97c224bfd621ec778f1c22e2
sghyun <sghyun@uwaterloo.ca>
2021-01-11 20:46:09 -0500

    separates the training of the tree to the results

---
+++ tree/generate_results.cpp
@@ -0,0 +1,158 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include "tree.h"
+#include "utility.h"
+#include "agent.h"
+#include <cmath>
+#include <string>
+using namespace std;
+
+string add(string s1, string s2){
+  ostringstream oss;
+  oss << s1 << s2;
+  return oss.str();
+}
+
+void read(Table &data, const string &fname){
+  ifstream ifs{fname};
+  string line;
+  while (getline(ifs,line)){
+    vector <double> v;
+    istringstream iss{line};
+    double val;
+    while (iss >> val){
+      v.emplace_back(val);
+      char c;
+      iss >> c;
+    }
+    data.push_back(v);
+  }
+}
+
+void run_test(Table &training, Table &test, Tuple &costs, string to_runs){ 
+
+  //int num_features = training[0].size();
+  int N = 10;
+  
+  vector<double> dist;
+  vector<double> dist_rand;
+  vector<double> dist_trained;
+  for (int f= 0; f < N; f++){
+    dist.emplace_back(0);
+    dist_rand.emplace_back(0);
+    dist_trained.emplace_back(0);
+  }
+  
+  cout << "starting pre-trained RL test" << endl;
+  for (int bc = 0; bc < N; bc++){
+    for (int i = 0; i < test.size(); i++){
+      // set everything to be unknown
+      auto t = test[i];
+      for (auto &h : t) h = -1;
+
+      double budget = (bc+1.0)/N;
+      string ap = add(to_string(bc+1), ".0.csv");
+      string fname = add(to_runs,ap);
+      trained_agent ta(fname);
+      ta.set_costs(&costs);
+      ta.set_budget(budget);
+      ta.set(&t, &test[i]);
+      ta.set_index(i);
+      while(ta.next_action()){} 
+      dist_trained[bc] += knn_total_distance(training, t, test[i], 5);
+    }
+  }
+
+  cout << "res trained:";
+  for (auto & d : dist_trained)
+    cout << " " << d;
+  cout << endl;
+  
+  cout << "starting pre-trained tree test" << endl;
+  for (int bc = 0; bc < N; bc++){
+    for (int i = 0; i < test.size(); i++){
+      // set everything to be unknown
+      auto t = test[i];
+      for (auto &h : t) h = -1;
+
+      double budget = (bc+1.0)/N;
+      string ap = add( add("_tree", to_string(bc+1))  , ".csv");
+      string fname = add(to_runs,ap);
+      trained_agent ta(fname);
+      ta.set_costs(&costs);
+      ta.set_budget(budget);
+      ta.set(&t, &test[i]);
+      ta.set_index(i);
+      while(ta.next_action()){} 
+      dist[bc] += knn_total_distance(training, t, test[i], 5);
+    }
+  } 
+  
+  cout << "res tree:";
+  for (auto & d : dist)
+    cout << " " << d;
+  cout << endl;
+  
+  cout << "starting random test" << endl;
+  for (int bc = 0; bc < N; bc++){
+    for (int i = 0; i < test.size(); i++){
+      // set everything to be unknown
+      auto t = test[i];
+      for (auto &h : t) h = -1;
+
+      double budget = (bc+1.0)/N;
+      random_agent ta;
+      ta.set_costs(&costs);
+      ta.set_budget(budget);
+      ta.set(&t, &test[i]);
+      while(ta.next_action()){} 
+      dist_rand[bc] += knn_total_distance(training, t, test[i], 5);
+    }
+  }
+  
+  cout << "res rand:";
+  for (auto & d : dist_rand)
+    cout << " " << d;
+  cout << endl;
+}
+
+int main(int argc, char *argv[]){
+  Table total_data;
+  srand(time(NULL));
+
+  Table training_data;
+  Table test_data;
+  vector<double> costs;
+  
+  string test_root = argv[1];
+  string training_fn = add(test_root, "_training.csv");
+  string test_fn = add(test_root, "_test.csv");
+  string costs_fn = add(test_root, "_cost.csv");
+  
+  cout << "read in files" << endl;
+
+  read(training_data, training_fn);
+  //random_shuffle(training_data.begin(), training_data.end());
+  read(test_data, test_fn);
+  int n = training_data[0].size();
+
+  ifstream cost_file(costs_fn);
+  string cost_line;
+  getline(cost_file, cost_line);
+  istringstream iss(cost_line);
+
+  for (long i = 0; i < n; i++){
+    char c;
+    double co;
+    iss >> co >> c;
+    costs.emplace_back(co);
+  }
+  for (auto &i : costs)
+    cout << " " << i;
+  cout << endl;
+
+  run_test(training_data, test_data, costs, argv[2]);
+}
--- tree/utility.cc
+++ tree/utility.cc
@@ -2,65 +2,102 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <algorithm>
 using namespace std;
 
 void normalize(Table &T){
-	auto num_tuples = T.size();
-	if (num_tuples == 0) return;
-	auto num_features = T[0].size();
+  auto num_tuples = T.size();
+  if (num_tuples == 0) return;
+  auto num_features = T[0].size();
 
-	for (int f = 0; f < num_features; f++){
-		// find the max and min
-		double max = T[0][f];
-		double min = T[0][f];
-		for (auto &t : T){
-			if (t[f] < 0) continue;
-			if (t[f] > max)
-				max = t[f];
-			if (t[f] < min)
-				min = t[f];
-		}
-		for (auto &t : T){
-			if (t[f] < 0) continue;
-			auto den = max - min;
-			if (den == 0) den = max;
-			t[f] = (t[f] - min) / (den);
-		}
+  for (int f = 0; f < num_features; f++){
+    // find the max and min
+    double max = T[0][f];
+    double min = T[0][f];
+    for (auto &t : T){
+      if (t[f] < 0) continue;
+      if (t[f] > max)
+	max = t[f];
+      if (t[f] < min)
+	min = t[f];
+    }
+    for (auto &t : T){
+      if (t[f] < 0) continue;
+      auto den = max - min;
+      if (den == 0) den = max;
+      t[f] = (t[f] - min) / (den);
+    }
 
-	}
+  }
 }
 
 void print(const Tuple &t){
-	for (auto &d : t)
-		cout << setprecision(2) << setw(5) << d << " ";
-	cout << endl;
+  for (auto &d : t)
+    cout << setprecision(2) << setw(5) << d << " ";
+  cout << endl;
 }
 
 void print(const Table &T){
-	for (auto &t : T)
-		print(t);
+  for (auto &t : T)
+    print(t);
 }
 
 double distance_l2(const vector<double> &v1, const vector<double> &v2){
-	double res = 0;
-	for (long i = 0; i < v1.size(); i++)
-		res += pow(v1[i] - v2[i],2);
-	return sqrt(res);
+  double res = 0;
+  for (long i = 0; i < v1.size(); i++)
+    res += pow(v1[i] - v2[i],2);
+  return sqrt(res);
 }
 
 // l2 distance only using known features
 double distance_l2_known(const vector<double> &v1, const vector<double> &v2){
-	double res = 0;
-	for (long i = 0; i < v1.size(); i++)
-		if (v1[i] >= 0 && v2[i] >= 0)
-			res += pow(v1[i] - v2[i],2);
-	return sqrt(res);
+  double res = 0;
+  for (long i = 0; i < v1.size(); i++)
+    if (v1[i] >= 0 && v2[i] >= 0)
+      res += pow(v1[i] - v2[i],2);
+  return sqrt(res);
 }
 
 vector<int> known_features(const Tuple &t){
-	vector<int> inds;
-	for (int i = 0; i < t.size(); i++)
-		if (t[i] != -1) inds.emplace_back(i);
-	return inds;
+  vector<int> inds;
+  for (int i = 0; i < t.size(); i++)
+    if (t[i] != -1) inds.emplace_back(i);
+  return inds;
 }
+
+// computes the total distance of k predicted closest elements to t_true
+double knn_total_distance(const Table &table, const Tuple &t, const Tuple &t_true, const int k){
+  vector<double> dists;
+  for (auto &d : table){
+    dists.emplace_back(distance_l2_known(t, d) );
+  }
+
+  double res = 0;                              
+  vector<int> already_in;      
+  for (int i = 0; i < k; i++){ 
+    double min_dist = dists[0];   
+    int ind = 0;  
+
+    for (int j = 1; j < dists.size(); j++){
+      auto d = dists[j];  
+      if (min_dist > d && find(already_in.begin(), already_in.end(), j) == already_in.end()){
+	min_dist = d;
+	ind = j;
+      }
+    }
+    res += distance_l2(t_true, table[ind]);
+    already_in.emplace_back(ind);
+  }
+  return res;
+}
+
+
+
+
+
+
+
+
+
+
 
